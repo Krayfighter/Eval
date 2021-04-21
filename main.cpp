753aecefe2b2136047c3818ff83adf3d69caf63c
Krayfighter <60051238+Krayfighter@users.noreply.github.com>
2021-04-21 11:19:48 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Term {
+	char sign;
+	float num;
+	Term(char s, float n) {
+		sign = s;
+		num = n;
+	}
+};
+
+class Equation {
+	vector<Term> content;
+	public:
+		add(char sign, float num) {
+			content.pushback(Term(sign, num))
+		}
+}
+
+int main()
+{
+	cout<<"Hello World";
+
+	return 0;
+}
