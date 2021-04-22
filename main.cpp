2ccbc7422eb3fbcccd9e70ee271659672cee8d0f
Aiden Kring <aidenjkring@gmail.com>
2021-04-22 11:20:52 -0400

    functional (no order of operations)

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ class Equation {
 	public:
 		Equation(vector<Term> ncontent = vector<Term>()) {content = ncontent;}
 		void add(char sign, double num) {content.push_back(Term(sign, num));}
+		void init() {add('+', 0.0);}
 		void print() {
 			for(int i = 0; i < content.size(); i++) {
 				cout << (char)content.at(i).sign << (int)content.at(i).num << endl;
@@ -35,35 +36,45 @@ class Equation {
 			for(int i = 1; i < content.size(); i++) {ncontent.push_back(Term(content[i].sign, content[i].num));}
 			return Equation(ncontent=ncontent);
 		}
+		vector<Term> *get_content() {return &content;};
 };
 
 double solve(Equation equ) {
-	switch(equ.at(1).sign) {
+
+	vector<Term> *cnt = equ.get_content();
+
+	switch(cnt->at(1).sign) {
 		case '+': {
-			if(equ.size() < 3) {return (double)(equ.at(0).num + equ.at(1).num);}
+			if(cnt->size() < 3) {return (double)(cnt->at(0).num + cnt->at(1).num);}
+			else {return (double)(cnt->at(0).num + solve(equ.rtrunk()));}
 		}break;
 		case '-': {
-			// 
+			if(cnt->size() < 3) {return (double)(cnt->at(0).num - cnt->at(1).num);}
+			else {return (double)(cnt->at(0).num - solve(equ.rtrunk()));}
 		}break;
 		case '*': {
-			// 
+			if(cnt->size() < 3) {return (double)(cnt->at(0).num * cnt->at(1).num);}
+			else {return (double)(cnt->at(0).num * solve(equ.rtrunk()));}
 		}break;
 		case '/': {
-			// 
+			if(cnt->size() < 3) {return (double)(cnt->at(0).num / cnt->at(1).num);}
+			else {return (double)(cnt->at(0).num / solve(equ.rtrunk()));}
 		}break;
 		default: {
-			raise(GenX);
-		}
+			throw(GenX());
+		}break;
 	}
+
 }
 
 int main() {
 
 	Equation equ;
+	equ.init();
 	equ.add('-', 3.0);
 	equ.add('*', 2.0);
 
-	equ = equ.rtrunk();
-	equ.print();
+	double out = solve(equ);
+	cout << out << endl;
 
 }
