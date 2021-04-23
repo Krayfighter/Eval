a884f73bc33a9fc4b0cf30ba73bea33e07efcc2b
Krayfighter <aidenjkring@gmail.com>
2021-04-23 09:58:31 -0400

    errors

--- main.cpp
+++ main.cpp
@@ -44,19 +44,22 @@ class Equation {
 			return Equation(ncontent=ncontent);
 		}
 		vector<Term> *get_content() {return &content;};
+		int size() {return content.size();}
 };
 
 Equation from_string(string str) {
-
-	Equation out = Equation();
-	out.init();
 
 	vector<string> tmp = {""};
 	char ops[4] = {'+', '-', '*', '/'};
 
 	for(int i = 0; i < str.size(); i++) {
-		if(find(begin(ops), end(ops), 'i') != end(ops)) {tmp.push_back(string(1, str[i]));}
-		else {tmp.at(tmp.size()) += str[i];}}
+		if(find(begin(ops), end(ops), str[i]) != end(ops)) {
+			tmp.push_back(string(1, str[i]));}
+		else {
+			tmp.at(tmp.size()-1) += str[i];}}
+	
+	Equation out = Equation();
+	out.init();
 
 	char sign;
 	string snum;
@@ -66,7 +69,14 @@ Equation from_string(string str) {
 
 		sign = var[0];
 		for(int i = 1; i < var.size(); i++) {snum += var[i];}
-		num = stod(snum);
+		try {
+			num = stod(snum);
+		}catch(invalid_argument Err) {
+			try {
+				num = stoi(snum);
+			}catch(invalid_argument err) {}
+		}
+		// num = (snum);
 		out.add(sign, num);
 
 		sign = char();
@@ -74,12 +84,15 @@ Equation from_string(string str) {
 
 	}
 
+	return out;
+
 }
 
 double solve(Equation equ) {
 
 	vector<Term> *cnt = equ.get_content();
 
+	// spaghetti code
 	switch(cnt->at(1).sign) {
 		case '+': {if(cnt->size() < 3) {return (double)(cnt->at(0).num + cnt->at(1).num);}
 			else {return (double)(cnt->at(0).num + solve(equ.rtrunk()));}}break;
@@ -89,19 +102,21 @@ double solve(Equation equ) {
 			else {return (double)(cnt->at(0).num * solve(equ.rtrunk()));}}break;
 		case '/': {if(cnt->size() < 3) {return (double)(cnt->at(0).num / cnt->at(1).num);}
 			else {return (double)(cnt->at(0).num / solve(equ.rtrunk()));}}break;
-		default: {throw(GenX());}break;
-	}
-
+		// default: {
+		// 	cout << cnt->at(1).sign << endl;
+		// 	// throw(GenX());}
+		// }break;
+		default: break;
 }
 
 int main() {
 
 	Equation equ = from_string("-3*2");
-	// equ.init();
-	// equ.add('-', 3.0);
-	// equ.add('*', 2.0);
 
+	// for(int i = 0; i < equ.size))
 	double out = solve(equ);
 	cout << out << endl;
+
+	return 0;
 
 }
