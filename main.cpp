d38d5d2e44072373970d429bba6395add0f4a56a
Krayfighter <aidenjkring@gmail.com>
2021-04-25 08:55:57 -0400

    better exmple program

--- main.cpp
+++ main.cpp
@@ -7,7 +7,20 @@ using namespace std;
 
 int main() {
 
-	Eval equ = Eval("3*2+4/6");
-	cout << equ.get_out() << endl;
+	// Eval equ = Eval("3*2+4/6");
+	// cout << equ.get_out() << endl;
+
+	while(1) {
+		string input;
+		cout << "enter equation: ";
+		cin >> input;
+		if(input == "exit") {
+			break;
+		}else {
+			cout << Eval(input).get_out() << endl;
+			continue;
+		}
+	}
+	return 0;
 
 }
